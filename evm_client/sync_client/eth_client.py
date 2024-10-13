403ec3efc3a152bae1dacb7c5414a931eb288e21
Ben <benjamin.g.sage@gmail.com>
2024-10-12 23:45:47 -0600

    client updates

--- evm_client/sync_client/eth_client.py
+++ evm_client/sync_client/eth_client.py
@@ -3,6 +3,14 @@ from evm_client.sync_client.client_core import SyncClientCore
 
 class SyncEthClient(SyncClientCore, EthCore):
 
+    def protocol_version(self, request_id: int=1):
+        body = self.get_eth_protocol_version_body(request_id=request_id)
+        return self.make_post_request(body)
+
+    def syncing(self, request_id: int=1):
+        body = self.get_eth_syncing_body(request_id=request_id)
+        return self.make_post_request(body)
+
     def block_number(self, request_id: int=1):
         body = self.get_eth_block_number_body(request_id=request_id)
         return self.make_post_request(body)

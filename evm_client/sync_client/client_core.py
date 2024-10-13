5fcc0c7e578b4925c3e1277e032ca7b3b670ad45
Ben <benjamin.g.sage@gmail.com>
2024-10-12 23:14:28 -0600

    updates

--- evm_client/sync_client/client_core.py
+++ evm_client/sync_client/client_core.py
@@ -5,7 +5,7 @@ class SyncClientCore:
     def __init__(self, node_url: str):
         self.url = node_url
 
-    def make_request(self, body):
+    def make_post_request(self, body):
         return requests.post(self.url, data=body)
     
     #batch request func?
--- evm_client/sync_client/eth_client.py
+++ evm_client/sync_client/eth_client.py
@@ -5,6 +5,6 @@ class SyncEthClient(SyncClientCore, EthCore):
 
     def block_number(self, request_id: int=1):
         body = self.get_eth_block_number_body(request_id=request_id)
-        return self.make_request(body)
+        return self.make_post_request(body)
 
 

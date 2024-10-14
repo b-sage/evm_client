e8b129e7d7ca5df92188cb03754df0084ac7b6af
Ben <benjamin.g.sage@gmail.com>
2024-10-14 00:39:57 -0600

    update tests

--- tests/sync_eth_client_test.py
+++ tests/sync_eth_client_test.py
@@ -12,29 +12,25 @@ def null_address():
 
 @pytest.fixture
 def block_num(client):
-    return int(client.block_number().json()['result'], 16)
+    return client.block_number()
 
 
 def test_get_block_number(client):
     block_number = client.block_number()
-    print(block_number.json())
-    assert type(block_number.json()) == dict
-    assert block_number.status_code == 200
+    print(block_number)
+    assert type(block_number) == int
 
 def test_protocol_version(client):
     protocol_version = client.protocol_version()
-    print(protocol_version.json())
-    assert type(protocol_version.json()) == dict
-    assert protocol_version.status_code == 200
+    print(protocol_version)
+    assert type(protocol_version) == str
 
 def test_syncing(client):
     syncing = client.syncing()
-    print(syncing.json())
-    assert type(syncing.json()) == dict
-    assert syncing.status_code == 200
+    print(syncing)
+    assert type(syncing) == bool
 
 def test_eth_get_balance(client, null_address, block_num):
     bal = client.get_balance(null_address, block_number=block_num)
-    print(bal.json())
-    assert type(bal.json()) == dict
-    assert bal.status_code == 200
+    print(bal)
+    assert type(bal) == int

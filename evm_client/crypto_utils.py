0ac3e473d96c30bb597ddd4265351889c5f72047
Ben <benjamin.g.sage@gmail.com>
2024-10-23 02:59:07 -0600

    add util

--- evm_client/crypto_utils.py
+++ evm_client/crypto_utils.py
@@ -1,3 +1,9 @@
+from eth_abi import decode
+from hexbytes import HexBytes
+from eth_utils import to_checksum_address
 
 def hex_to_int(hex_str: str):
     return int(hex_str, 16)
+
+def unpack_address(packed_address: str):
+    return to_checksum_address(decode(['address'], packed_address)[0])

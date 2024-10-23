176e2a4eb1aafb17e4f8b50c462146ee1c5c2668
Ben <benjamin.g.sage@gmail.com>
2024-10-23 11:29:19 -0600

    fix

--- evm_client/crypto_utils.py
+++ evm_client/crypto_utils.py
@@ -6,4 +6,4 @@ def hex_to_int(hex_str: str):
     return int(hex_str, 16)
 
 def unpack_address(packed_address: str):
-    return to_checksum_address(decode(['address'], packed_address)[0])
+    return to_checksum_address(decode(['address'], HexBytes(packed_address))[0])

c4487061b7d00494dab7d0286e0a4b18983d962e
Ben <benjamin.g.sage@gmail.com>
2024-10-24 11:43:36 -0600

    update

--- evm_client/errors.py
+++ evm_client/errors.py
@@ -1,8 +1,12 @@
 
-class HTTPStatusError(BaseException):
+class HTTPStatusError(Exception):
     pass
 
 #TODO: should get a bit more specific with these. Can be due to bad input, node issues, etc
-class NodeError(BaseException):
-    pass
+class NodeError(Exception):
+    
+    def __init__(self, error_msg, request_id):
+        super().__init__(error_msg)
+        self.error_msg = error_msg
+        self.request_id=request_id
 

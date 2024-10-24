a44f3a9067b04b78fbd92bbcb421616855ab5c53
Ben <benjamin.g.sage@gmail.com>
2024-10-24 14:39:03 -0600

    debug

--- evm_client/batch_client/utils.py
+++ evm_client/batch_client/utils.py
@@ -15,6 +15,7 @@ def process_batch_http_response(resp: Response):
         results = resp.json()
         results_ = {}
         for r in results:
+            print(r)
             _id = r['id']
             if r.get('error'):
                 raise NodeError(r['error'], _id, results=results_)

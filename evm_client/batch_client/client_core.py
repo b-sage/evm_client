7b89209369f0639d3ce828886a1bd5eb93bdc0e1
Ben <benjamin.g.sage@gmail.com>
2024-10-28 16:43:18 -0600

    debug

--- evm_client/batch_client/client_core.py
+++ evm_client/batch_client/client_core.py
@@ -12,6 +12,7 @@ class BatchClientCore(SyncClientCore):
                 try:
                     yield process_batch_http_response(res)
                 except NodeError as n:
+                    print(n.request_id)
                     chunked_requests[i] = [c for c in chunk if c['request_id'] != n.request_id]
                     return self._execute_drop_reverts(chunked_requests)
 

f2906388235816b20e3a86ade3809cbaa1a595ba
Ben <benjamin.g.sage@gmail.com>
2024-10-29 16:01:59 -0600

    update parsers

--- evm_client/parsers/hex_to_int.py
+++ evm_client/parsers/hex_to_int.py
@@ -1,17 +1,7 @@
-from evm_client.parsers.base import BaseParser
 from evm_client.crypto_utils import hex_to_int
-from evm_client.parsers.utils import assert_int
 
 def parse_raw_hex_to_int(hex_str: str):
-    return HexToIntParser(hex_to_int(hex_str))
+    if hex_str:
+        return hex_to_int(hex_str)
+    return
 
-class HexToIntParser(BaseParser):
-
-    def __init__(self, value: int):
-        assert_int(self.value)
-        self.value = value
-
-    def default_format(self):
-        if self.value:
-            return self.value
-        return

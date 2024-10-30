634e96fad82f327fb5e965fa74a354ccf7028602
Ben <benjamin.g.sage@gmail.com>
2024-10-29 19:13:14 -0600

    update

--- evm_client/parsers/utils.py
+++ evm_client/parsers/utils.py
@@ -4,3 +4,13 @@ def assert_int(item):
 
 def assert_str(item):
     assert type(item) == str or item is None
+
+def underscore_to_camelcase(value):
+    def camelcase(): 
+        yield str.lower
+        while True:
+            yield str.capitalize
+
+    c = camelcase()
+    return "".join(next(c)(x) if x else '_' for x in value.split("_"))
+
